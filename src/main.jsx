0ffc57565d05c3868086ff19abb0c45fd14a463d
GMaxDev <maxime.guigra@gmail.com>
2024-05-08 22:56:11 +0200

    fichier scss de base

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './assets/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1>test</h1>
   </React.StrictMode>,
 )
